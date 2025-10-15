c88476e70b6125752f3f1c568394d37aa5919e08
Olaf Loewe <olafloewe95@gmail.com>
2025-10-15 19:24:08 +0200

    fancy way of asking for parameters

---
+++ Task 2/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_2 {
+    internal class Program {
+        public static double[] PolyRoots(double[] coefficients) { 
+            return coefficients;
+        }
+
+        public static void Main(string[] args) {
+
+            List<double> nums = new List<double>();
+            string source;
+
+            // loop to ask for arguments
+            do{
+                double number;
+                Console.Clear();
+                Console.Write("Coefficients so far: ");
+
+                Console.Write("[ ");
+                foreach (double i in nums) {
+                    Console.Write($"{i}, ");
+                }
+                Console.Write("]\n");
+
+                Console.WriteLine("Please enter positive or negative coefficients: \nEnter 'Q' or 'q' to stop entry");
+                source = Console.ReadLine();
+
+                if(!double.TryParse(source, out number)) continue;
+
+                nums.Add(number);
+            } while (source.ToLower() != "q");
+
+            foreach (double i in PolyRoots(nums.ToArray())) { 
+                Console.WriteLine(i);
+            }
+
+            // HOLD THE LINE (terminal window) !!!
+            Console.ReadLine();
+        }
+    }
+}
